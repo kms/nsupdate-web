3171c95182b85c99bf0f471bd6b14d3697e58171
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- nsupdate-web-telnet.sh
+++ nsupdate-web-telnet.sh
@@ -2,7 +2,7 @@
 # nsupdate-web
 # Updates BIND records.
 #
-# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
 
 # Crude examples
